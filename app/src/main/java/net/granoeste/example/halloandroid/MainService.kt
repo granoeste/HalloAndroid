f8f271d7d10769ef531f82fc2a5b387e55630ba1
Katsumi Onishi <granoeste@gmail.com>
2020-07-27 17:44:30 +0900

    Add MainService

---
+++ app/src/main/java/net/granoeste/example/halloandroid/MainService.kt
@@ -0,0 +1,12 @@
+package net.granoeste.example.halloandroid
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MainService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
